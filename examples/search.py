7a2b9565b935779d811485f28b4a36e2500423c1
Mani Arasteh <mani.arasteh92@gmail.com>
2026-07-28 00:31:21 +0330

    Add examples for the new SearchItem

--- examples/search.py
+++ examples/search.py
@@ -1,4 +1,5 @@
 from chevron import Search
+from chevron.ui.search_control import SearchItem
 
 
 packages = [
@@ -17,3 +18,27 @@ package = Search(
 ).ask()
 
 print(f"Selected: {package}")
+
+pacman = Search(
+    "Choose your package manager",
+    [
+    SearchItem(
+        value="pip",
+        label="pip",
+        description="Python's default package manager"
+    ),
+    SearchItem(
+        value="poetry",
+        label="Poetry",
+        description="A good package manager"
+    ),
+    SearchItem(
+        value="uv",
+        label="uv",
+        description="An even better package manager"
+    )
+    ]
+).ask()
+
+print(f"Selected: {pacman}")
+
